bbd430454e61fb02eaf9cd63af733a7b5582f311
nayeem-miah <nayeem5113a@gmail.com>
2025-08-20 19:37:19 +0600

    part-1 done

--- src/components/layout/CommonLayout.tsx
+++ src/components/layout/CommonLayout.tsx
@@ -10,7 +10,7 @@ interface IProps {
 
 export default function CommonLayout({ children }: IProps) {
     return (
-        <div className="min-h-screen bg-red-500 flex flex-col">
+        <div className="min-h-screen  flex flex-col">
             <Navbar />
             <div className="grow-1">{children} </div>
             <Footer />
