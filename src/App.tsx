c992add761b315358afb78adc65186415237d2ff
nayeem-miah <nayeem5113a@gmail.com>
2025-08-20 12:47:00 +0600

    initial setup project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+
+    <div>
+      <h3>This is a simple react application</h3>
+      <Button>Click me </Button>
+    </div>
+  )
+}
+
+export default App
