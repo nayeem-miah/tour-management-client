4157a65fc888a1802af093ee2b5defcbff0da5be
nayeem-miah <nayeem5113a@gmail.com>
2025-08-20 19:34:49 +0600

    part-1 done

--- src/components/layout/CommonLayout.tsx
+++ src/components/layout/CommonLayout.tsx
@@ -1,7 +1,7 @@
 
 import type { ReactNode } from "react"
-import Navbar from "./Navbar"
 import Footer from "./Footer"
+import Navbar from "./Navbar"
 
 interface IProps {
     // children: ReactElement
@@ -10,9 +10,9 @@ interface IProps {
 
 export default function CommonLayout({ children }: IProps) {
     return (
-        <div>
+        <div className="min-h-screen bg-red-500 flex flex-col">
             <Navbar />
-            {children}
+            <div className="grow-1">{children} </div>
             <Footer />
         </div>
     )
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,10 @@
 
 export default function About() {
     return (
-        <div>About</div>
+        <div className="py-16 px-4 container mx-auto">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, blanditiis! Minima animi laborum earum minus incidunt maxime similique. Harum voluptatum atque recusandae at nesciunt voluptatem porro vero perferendis, corporis officia?
+            diev
+            div
+        </div>
     )
 }
