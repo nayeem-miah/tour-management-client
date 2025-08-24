228e758c8ee2bdeca614f4111b727535632d353a
nayeem-miah <nayeem5113a@gmail.com>
2025-08-24 12:20:39 +0600

    dashboard

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,77 @@
+import * as React from "react"
+
+import Logo from "@/assets/icons/Logo"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+import { Link } from "react-router"
+
+// This is sample data.
+const data = {
+  versions: ["1.0.1", "1.1.0-alpha", "2.0.0-beta1"],
+  navMain: [
+    {
+      title: "Dashboard",
+      url: "#",
+      items: [
+        {
+          title: "analytics",
+          url: "/admin/analytics",
+        }
+      ],
+    },
+    {
+      title: "Tour Management",
+      url: "#",
+      items: [
+        {
+          title: "add tour",
+          url: "/admin/add-tour",
+        },
+        {
+          title: "Tour Types",
+          url: "/admin/tour-types",
+        }
+      ],
+    },
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar {...props}>
+      <SidebarHeader>
+        <Logo />
+      </SidebarHeader>
+      <SidebarContent>
+        {/* We create a SidebarGroup for each parent. */}
+        {data.navMain.map((item) => (
+          <SidebarGroup key={item.title}>
+            <SidebarGroupLabel>{item.title}</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {item.items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild >
+                      <Link to={item.url}>{item.title}</Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        ))}
+      </SidebarContent>
+      <SidebarRail />
+    </Sidebar>
+  )
+}
--- src/routes/Index.tsx
+++ src/routes/Index.tsx
@@ -1,7 +1,11 @@
 import App from "@/App";
+import DashboardLayout from "@/components/layout/DashboardLayout";
 import About from "@/pages/About";
+import AddTour from "@/pages/admin/AddTour";
+import Analytics from "@/pages/admin/Analytics";
 import Login from "@/pages/Login";
 import Register from "@/pages/Register";
+import Bookings from "@/pages/user/Bookings";
 import Verify from "@/pages/Verify";
 import { createBrowserRouter } from "react-router";
 
@@ -16,6 +20,37 @@ export const router = createBrowserRouter([
             }
         ]
     },
+
+    {
+        Component: DashboardLayout,
+        path: "/admin",
+        children: [
+            {
+                Component: Analytics,
+                // path: "/admin/analytics"
+                path: "analytics"
+            },
+            {
+                Component: AddTour,
+                path: "add-tour"
+            },
+            {
+                Component: AddTour,
+                path: "add-types"
+            },
+        ]
+    },
+    {
+        Component: DashboardLayout,
+        path: "/user",
+        children: [
+            {
+                Component: Bookings,
+                path: "bookings"
+            }
+        ]
+    },
+
     {
         Component: Login,
         path: "/login"
