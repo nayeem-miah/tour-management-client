bba72cd2a98395df84375fa7eb2e5e27d1502f45
nayeem-miah <nayeem5113a@gmail.com>
2025-08-28 17:54:21 +0600

    date piker

--- src/pages/admin/AddTour.tsx
+++ src/pages/admin/AddTour.tsx
@@ -1,12 +1,17 @@
 
 import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
+import { cn } from "@/lib/utils";
 import { useGetDivisionQuery } from "@/redux/features/division/division.api";
 import { useGetTourTypesQuery } from "@/redux/features/tour/tour.api";
+import { format, formatISO } from "date-fns";
+import { CalendarIcon } from "lucide-react";
 import { useForm, type FieldValues, type SubmitHandler } from "react-hook-form";
 
 export default function AddTour() {
@@ -24,18 +29,26 @@ export default function AddTour() {
     }))
 
 
-    console.log(divisionOptions);
+    console.log(tourTypes);
     const form = useForm({
         defaultValues: {
             title: "",
             division: "",
             tourType: "",
-            description: ""
+            description: "",
+            startDay: "",
+            endDay: ""
         }
     });
 
-    const onSubmit: SubmitHandler<FieldValues> = async (formData) => {
-        console.log(formData);
+    const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+
+        const tourDate = {
+            ...data,
+            startDay: formatISO(data.startDay),
+            endDay: formatISO(data.endDay),
+        }
+        console.log(tourDate);
     }
 
 
@@ -131,7 +144,94 @@ export default function AddTour() {
                                     )}
                                 />
                             </div>
-
+                            <div className="flex gap-5">
+                                <FormField
+                                    control={form.control}
+                                    name="startDay"
+                                    render={({ field }) => (
+                                        <FormItem className="flex flex-col flex-1">
+                                            <FormLabel>Date of birth</FormLabel>
+                                            <Popover>
+                                                <PopoverTrigger asChild>
+                                                    <FormControl>
+                                                        <Button
+                                                            variant={"outline"}
+                                                            className={cn(
+                                                                "w-full pl-3 text-left font-normal",
+                                                                !field.value && "text-muted-foreground"
+                                                            )}
+                                                        >
+                                                            {field.value ? (
+                                                                format(field.value, "PPP")
+                                                            ) : (
+                                                                <span>Pick a date</span>
+                                                            )}
+                                                            <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                                        </Button>
+                                                    </FormControl>
+                                                </PopoverTrigger>
+                                                <PopoverContent className="w-auto p-0" align="start">
+                                                    <Calendar
+                                                        mode="single"
+                                                        selected={new Date(field.value)}
+                                                        onSelect={field.onChange}
+                                                        disabled={(date) =>
+                                                            date < new Date(
+                                                                new Date().setDate(new Date().getDate() - 1)
+                                                            )
+                                                        }
+                                                        captionLayout="dropdown"
+                                                    />
+                                                </PopoverContent>
+                                            </Popover>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    control={form.control}
+                                    name="endDay"
+                                    render={({ field }) => (
+                                        <FormItem className="flex flex-col flex-1">
+                                            <FormLabel>End date</FormLabel>
+                                            <Popover>
+                                                <PopoverTrigger asChild>
+                                                    <FormControl>
+                                                        <Button
+                                                            variant={"outline"}
+                                                            className={cn(
+                                                                "w-full pl-3 text-left font-normal",
+                                                                !field.value && "text-muted-foreground"
+                                                            )}
+                                                        >
+                                                            {field.value ? (
+                                                                format(field.value, "PPP")
+                                                            ) : (
+                                                                <span>Pick a date</span>
+                                                            )}
+                                                            <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                                        </Button>
+                                                    </FormControl>
+                                                </PopoverTrigger>
+                                                <PopoverContent className="w-auto p-0" align="start">
+                                                    <Calendar
+                                                        mode="single"
+                                                        selected={new Date(field.value)}
+                                                        onSelect={field.onChange}
+                                                        captionLayout="dropdown"
+                                                        disabled={(date) =>
+                                                            date < new Date(
+                                                                new Date().setDate(new Date().getDate() - 1)
+                                                            )
+                                                        }
+                                                    />
+                                                </PopoverContent>
+                                            </Popover>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                            </div>
                             <FormField
                                 control={form.control}
                                 name="description"
@@ -151,7 +251,7 @@ export default function AddTour() {
 
                         </form>
                     </Form>
-                    <Button className="" type="submit" form="add-tour">Submit</Button>
+                    <Button className="w-full mt-3" type="submit" form="add-tour">Submit</Button>
                 </CardContent>
             </Card>
         </div>
