476bcd0591e23b41d08f704472ea192542646895
nayeem-miah <nayeem5113a@gmail.com>
2025-09-01 19:58:55 +0600

    payment success

--- src/routes/Index.tsx
+++ src/routes/Index.tsx
@@ -16,6 +16,9 @@ import Tours from "@/pages/Tour";
 import Bookings from "@/pages/user/Bookings";
 import TourDetails from "@/pages/TourDetails";
 import HomePage from "@/pages/HomePage";
+import Success from "@/pages/payment/Success";
+import Fail from "@/pages/payment/Fail";
+import Cancel from "@/pages/payment/Cancel";
 
 export const router = createBrowserRouter([
     {
@@ -76,6 +79,18 @@ export const router = createBrowserRouter([
     {
         Component: Unauthorized,
         path: "/unauthorized"
+    },
+    {
+        Component: Success,
+        path: "/payment/success"
+    },
+    {
+        Component: Fail,
+        path: "/payment/fail"
+    },
+    {
+        Component: Cancel,
+        path: "/payment/cancel"
     },
 ]);
 
