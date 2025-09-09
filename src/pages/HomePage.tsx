98ae709fc903d150001f8f7495ce66b789b2e282
nayeem-miah <nayeem5113a@gmail.com>
2025-09-09 14:43:19 +0600

    embedted url

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,6 +5,9 @@ export default function HomePage
     return (
         <div>
             <HeroSection />
+            <div>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/6KMiCl-BgJ4?si=fEvC8T51PNZeWNBq" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
         </div>
     )
 }
